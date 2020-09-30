9e427fad39fd3351c48616a90f49d241326acc9d
Votre nom <login@domaine.example>
2020-09-30 00:38:48 -0700

    test

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,18 @@ Route::get('/centre_interet', function () {
 Route::get('/contact', function () {
     return view('Contact');
 });
+
+
+Route::get('/test', function () {
+
+$pdo = DB::connection()->getPdo();
+
+if($pdo)
+   {
+     echo "Connected successfully to database ".DB::connection()->getDatabaseName();
+    DB::table('Formulaire')->insert(
+    array('Nom' => 'Gilbert', 'Entreprise' => 'ikea','Email'=>'ok@gmail.com', 'Message'=>'Hello their my guy'));
+   } else {
+     echo "You are not connected to database";
+   }
+});
